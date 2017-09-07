c3620d7780270afa2b3fb75ab6bd4599b24fc67f
Richard Hwang <richard@doordash.com>
2017-09-07 15:48:16 -0700

    Add more details

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='geohash_polygon',
+      version='0.1',
+      description='',
+      url='https://github.com/rhwang201/geohash_polygon',
+      author='Richard Hwang',
+      author_email='richard.hwang201@gmail.com',
+      license='MIT',
+      packages=['geohash_polygon'],
+      zip_safe=False)
